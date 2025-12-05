7da378e9cfdd3d391dca1f2ca5a71ec833478dec
widgeiw <crazynas228@gmail.com>
2025-12-05 06:15:51 +0300

    [TASK-5] Registering decorator, multiplex and separator and adding send and recv

--- orlova.alena/task-5/pkg/conveyer/conveyer.go
+++ orlova.alena/task-5/pkg/conveyer/conveyer.go
@@ -2,6 +2,7 @@ package conveyer
 
 import (
 	"context"
+	"fmt"
 )
 
 type conveyer interface {
@@ -30,7 +31,6 @@ type ConveyerImpl struct {
 	decorators   []decoratorSpec
 	multiplexers []multiplexerSpec
 	separators   []separatorSpec
-	stop         chan struct{}
 }
 
 type decoratorSpec struct {
@@ -49,4 +49,81 @@ type multiplexerSpec struct {
 	fn     func(ctx context.Context, inputs []chan string, output chan string) error
 	inputs []string
 	output string
+}
+
+func New(size int) conveyer {
+	return &ConveyerImpl{
+		channels: make(map[string]chan string),
+	}
+}
+
+func (conv *ConveyerImpl) createChannel(id string) {
+	if _, exists := conv.channels[id]; !exists {
+		conv.channels[id] = make(chan string, 10)
+	}
+}
+
+func (conv *ConveyerImpl) getChannel(id string) (chan string, error) {
+	ch, exists := conv.channels[id]
+	if !exists {
+		return nil, fmt.Errorf("chan not found")
+	}
+	return ch, nil
+}
+
+func (conv *ConveyerImpl) RegisterDecorator(
+	fn func(ctx context.Context, input chan string, output chan string) error,
+	input string,
+	output string,
+) {
+	conv.decorators = append(conv.decorators, decoratorSpec{
+		fn:     fn,
+		input:  input,
+		output: output,
+	})
+}
+
+func (conv *ConveyerImpl) RegisterMultiplexer(
+	fn func(ctx context.Context, inputs []chan string, output chan string) error,
+	inputs []string,
+	output string,
+) {
+	conv.multiplexers = append(conv.multiplexers, multiplexerSpec{
+		fn:     fn,
+		inputs: inputs,
+		output: output,
+	})
+}
+
+func (conv *ConveyerImpl) RegisterSeparator(
+	fn func(ctx context.Context, input chan string, outputs []chan string) error,
+	input string,
+	outputs []string,
+) {
+	conv.separators = append(conv.separators, separatorSpec{
+		fn:      fn,
+		input:   input,
+		outputs: outputs,
+	})
+}
+
+func (c *ConveyerImpl) Send(input string, data string) error {
+	ch, err := c.getChannel(input)
+	if err != nil {
+		return err
+	}
+	ch <- data
+	return nil
+}
+
+func (c *ConveyerImpl) Recv(output string) (string, error) {
+	ch, err := c.getChannel(output)
+	if err != nil {
+		return "", err
+	}
+	data, ok := <-ch
+	if !ok {
+		return "undefined", nil
+	}
+	return data, nil
 }
